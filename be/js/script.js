600fc876756afebd35f7abbd1071c24b441cd30a
Vadim <61314487+Vadin322@users.noreply.github.com>
2021-11-20 19:05:53 +0200

    Add files via upload

---
+++ be/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.sliders').slick({  
+     arrows: false,
+     dots: true
+});
+});;
+$(document).ready(function(){
+    $('.second_sliders').slick({  
+        dots: true, 
+        arrows: true, 
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 2  
+});
+});
+    
